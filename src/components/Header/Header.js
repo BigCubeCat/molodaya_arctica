ca57b27e0ca5654e620c119b7708ec8b4a106dfa
BigCubeCat <bigcubecat@yandex.ru>
2022-07-14 22:56:29 +0700

    Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import {Text} from '@rneui/themed';
+import { Header as HeaderRNE, HeaderProps, Icon } from '@rneui/themed';
 
 
 const Header = () => {
     return (
         <View style={styles.container}>
-            <Text>Header!</Text>
+            <HeaderRNE
+                leftComponent={{
+                    icon: 'menu',
+                    color: '#fff',
+                }}
+                centerComponent={{ text: 'Header', style: styles.heading }}
+            >
+            </HeaderRNE>
         </View>
     );
 };
@@ -15,6 +23,19 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor:'grey',
         height:100,
+    },
+    headerContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#397af8',
+        marginBottom: 20,
+        width: '100%',
+        paddingVertical: 15,
+    },
+    heading: {
+        color: 'white',
+        fontSize: 22,
+        fontWeight: 'bold',
     },
 });
 
