93fc40a270f4b76000f7ff89bec5fb48160a169d
BigCubeCat <bigcubecat@yandex.ru>
2022-07-15 06:06:04 +0700

    show messages (not finished)

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,18 +1,30 @@
 import {ScrollView} from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View} from 'react-native';
-import {Button, Icon, Input} from '@rneui/base';
+import {Button, Icon, Input, Text} from '@rneui/base';
 import {StyleSheet} from 'react-native-web';
-import {sendMessage} from '../../utils/chatClient';
-import {AppContext} from "../../../App";
+import {getChat, sendMessage} from '../../utils/chatClient';
+import {AppContext} from '../../../App';
 
 export default function Chat({chat_id = '0'}) {
-  const {isSignedIn, setIsSignedIn, user, setUser} = React.useContext(AppContext)
+  const {isSignedIn, setIsSignedIn, user, setUser} = React.useContext(
+      AppContext);
   const [message, setMessage] = useState('');
+  const [allMessages, setAllMessages] = useState([]);
+  useEffect(() => {
+    const fetchReq = async () => {
+      let [data, error] = await getChat(chat_id);
+      console.log(data, error)
+      setAllMessages(data);
+    };
+    fetchReq().catch(console.error);
+  }, [message]);
   return (
       <View>
         <ScrollView>
-
+          {allMessages.map(mess => {
+            return <Text>{mess.message}</Text>;
+          })}
         </ScrollView>
         <Input
             style={styles.input}
@@ -25,10 +37,10 @@ export default function Chat({chat_id = '0'}) {
               const fetchRequest = async () => {
                 let {data, error} = await sendMessage(chat_id, user, message);
                 if (error) {
-                  console.error(error)
+                  console.error(error);
                 }
-              }
-              fetchRequest().then(setMessage("")).catch(console.error)
+              };
+              fetchRequest().then(setMessage('')).catch(console.error);
             }}
         >Отправить <Icon name="send" color={'white'}/>
         </Button>
