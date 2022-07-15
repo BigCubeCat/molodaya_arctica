3e4e39f7ff090ef3e1df844cb4fd0ce558aa1cfb
Konstantin R <konstantin_rebrov@epam.com>
2022-07-15 11:22:15 +0300

    log off menu

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -1,18 +1,47 @@
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {
+    createDrawerNavigator, DrawerContentScrollView,
+    DrawerItemList, DrawerItem
+} from '@react-navigation/drawer';
 import Home from '../Home/Home';
 import React from 'react';
 import EventsPage from '../Page/EventsPage';
 import NewsSVG from '../SVG/news';
 import EventsSVG from '../SVG/events';
 import SpacesPage from '../Page/SpacesPage';
+import {AsyncStorage, Text, View} from "react-native";
+import {AppContext} from "../../../App";
 import AllPage from '../Page/AllPage';
 import JobPage from '../Page/JobPage';
 
 const DrawerNav = createDrawerNavigator();
 
+function CustomDrawerContent(props) {
+    const {setIsSignedIn} = React.useContext(AppContext)
+
+    const logOff = async () => {
+        await AsyncStorage.removeItem("access_token")
+        setIsSignedIn(false)
+    }
+    return (
+        <DrawerContentScrollView {...props} contentContainerStyle={{flex: 1, justifyContent: 'space-between'}}>
+            <View>
+                <DrawerItemList {...props} />
+            </View>
+            <View style={{
+                backgroundColor: "#383B46", bottom: 0, position: 'absolute', width: '100%' }}>
+                <DrawerItem label={() =>
+                    <Text style={{color: 'white'}}>Выйти</Text>}
+                            onPress={logOff}
+                ></DrawerItem>
+            </View>
+        </DrawerContentScrollView>
+    );
+}
+
+
 const Drawer = () => {
   return (
-      <DrawerNav.Navigator>
+      <DrawerNav.Navigator  drawerContent={(props) => <CustomDrawerContent {...props} />}>
         <DrawerNav.Screen name="Актуальное" component={AllPage} options={{
           drawerIcon: ({focused, size}) => (
               <NewsSVG/>
