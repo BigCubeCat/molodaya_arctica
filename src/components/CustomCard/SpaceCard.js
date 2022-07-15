7d0a71b67ad3179065471b9840c1e829ac32a970
BigCubeCat <bigcubecat@yandex.ru>
2022-07-15 15:14:00 +0700

    spaces

--- src/components/CustomCard/SpaceCard.js
+++ src/components/CustomCard/SpaceCard.js
@@ -2,7 +2,8 @@ import {View} from 'react-native';
 import {StyleSheet} from 'react-native-web';
 import {Card, Image} from '@rneui/themed';
 import {Button, Icon, Text} from '@rneui/base';
-import {Avatar} from '@rneui/base/dist/Avatar/Avatar';
+import RenderHtml from 'react-native-render-html';
+
 
 export default function SpaceCard(
     {
@@ -10,21 +11,26 @@ export default function SpaceCard(
       address = 'Мурманская обл, г Апатиты, ул Фестивальная, д 23А',
       imageUri = 'https://randomuser.me/api/portraits/men/36.jpg',
       description = 'В городе Кандалакша, современное молодежное пространство «Arctic Space», концепцию которого модернизирована и разработана Дизайн-заводом "Флакон" (г. Москва)',
-        user=""
+      caption = '',
+      callback = () => {
+      },
+        single=false,
     },
 ) {
   return (
       <Card containerStyle={styles.card}>
         <View style={styles.row}>
-          <Card.Image  source={imageUri} />
+          <Card.Image source={imageUri}/>
           <View style={styles.title}>
             <Text>{title}</Text>
             <Text style={styles.address}>{address}</Text>
           </View>
         </View>
         <Text style={styles.description}>{description}</Text>
+        {(caption.length > 0) ? <RenderHtml source={{html: caption}} /> : null}
         <Button
             type="clear"
+            onPress={callback}
             buttonStyle={{
               borderRadius: 0,
               marginLeft: 0,
@@ -32,8 +38,7 @@ export default function SpaceCard(
               marginBottom: 0,
             }}
         >
-          Подробнее
-          <Icon name="arrow-right" color={'#397af8'}/>
+          {(single) ? "Закрыть": "Подробнее"}
         </Button>
       </Card>
   );
@@ -55,7 +60,7 @@ const styles = StyleSheet.create({
   address: {
     color: '#565862',
     fontSize: 10,
-    maxWidth: '80%'
+    maxWidth: '80%',
   },
   description: {
     color: '#565862',
--- src/components/Page/SpacesPage.js
+++ src/components/Page/SpacesPage.js
@@ -5,22 +5,39 @@ import SpaceCard from '../CustomCard/SpaceCard';
 
 export default function SpacesPage() {
   const [events, setEvents] = useState([]);
+  const [currentSpace, setCurrenSpace] = useState(null);
   useEffect(async () => {
     const result = await fetchAPI(
         'https://molodaya-arctica.ru/api/content/spaces?page=1');
     setEvents(result.resources);
   }, []);
-  return (
-      <ScrollView>
-        {events.map(poster => {
-          return <SpaceCard
-              id={"s"+poster.id}
-              title={poster.title}
-              address={poster.address.title}
-              imageUri={poster.media[0].path}
-              description={poster.caption}
-          />;
-        })}
-      </ScrollView>
-  );
+  console.log(events);
+  if (currentSpace === null) {
+    return (
+        <ScrollView>
+          {events.map((poster, poster_num) => {
+            return <SpaceCard
+                id={'s' + poster.id}
+                title={poster.title}
+                address={poster.address.title}
+                imageUri={poster.media[0].path}
+                description={poster.caption}
+                callback={() => setCurrenSpace(poster_num)}
+            />;
+          })}
+        </ScrollView>
+    );
+  } else {
+    const poster = events[currentSpace];
+    return <ScrollView><SpaceCard
+        id={'s' + poster.id}
+        title={poster.title}
+        address={poster.address.title}
+        imageUri={poster.media[0].path}
+        caption={poster.entity.content}
+        description={poster.caption}
+        single={true}
+        callback={() => setCurrenSpace(null)}
+    /></ScrollView>;
+  }
 }
